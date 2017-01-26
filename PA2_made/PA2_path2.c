39af17227f9dd32e5734ac08e0a9885cb2eb9078
Mitch <gainesmitch2@gmail.com>
2017-01-25 23:13:24 -0500

    input values centered in grid

--- PA2_made/PA2_path2.c
+++ PA2_made/PA2_path2.c
@@ -7,23 +7,49 @@
 
 
 FILE *input;
-int x, y, gens;
+int x, y, gens, init_x = 0, init_y = 0;
 char buff[255];
 int print, pause;
 int** grid; 
 
 
 void allocate_2d_mem(void){
-//allocates memory for the 2d array
+	//allocates memory for the 2d array
 	grid = (int **)malloc(y*sizeof(int*));
 	for(int c = 0; c < y; c++) grid[c] = (int*)malloc(x*sizeof(int));
 }//allocate_2d_mem
 
-void initialize_values(void){
-//initialize values in grid[][] to ASCII value of "o"
+void initialize_values(FILE *init_config){
+	//initialize values in grid[][] to ASCII value of "o"
 	for(int i = 0; i < y; i++)
 		for(int j = 0; j < x; j++)
 			grid[i][j] = (int) 111;
+
+
+	//overwrite with init configeration and center it in grid
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int x_assign = 0, y_assign = 0, x_start = 0, y_start = 0;
+
+
+	for(int i = 0; i < 2; i++) {
+		while((read = getline(&line, &len, init_config)) != -1){
+			if(!i) { // gets length and width of file matrix
+				init_x = !init_y ? read-1 : init_x; 
+				init_y++;
+			} else { // assigns init values to center of grid array
+				for(x_assign = x_start; x_assign < init_x + x_start; x_assign++)
+					grid[y_assign][x_assign] = line[x_assign-x_start];
+				y_assign++;	
+			}
+		}
+		rewind(init_config);//go back to beginning of file
+		x_start = (int) x/2 - init_x/2;
+		y_start = (int) y/2 - init_y/2;
+		y_assign = y_start;
+	}
+
 }//initialize_values
 
 void print_init(void){
@@ -50,8 +76,9 @@ int main(int argc, char *argv[]) {
 	
 	
 	allocate_2d_mem();
-	initialize_values();
+	initialize_values(input);
 	print_init();
+	
 
 	fclose(input);
 
