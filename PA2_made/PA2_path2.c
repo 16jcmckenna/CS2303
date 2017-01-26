8e942a337eb6ebb15ca75e1c6dec03b44f902096
Mitch <gainesmitch2@gmail.com>
2017-01-25 22:42:17 -0500

    making init grid with mem_alloc 2d array

--- PA2_made/PA2_path2.c
+++ PA2_made/PA2_path2.c
@@ -10,14 +10,31 @@ FILE *input;
 int x, y, gens;
 char buff[255];
 int print, pause;
+int** grid; 
 
 
-
-void allocate_2d_mem(int*** grid){
+void allocate_2d_mem(void){
 //allocates memory for the 2d array
-	*grid = (int**)malloc(x*sizeof(int*));
-	for(int c = 0; c < x; c++) (*grid)[c] = (int*)malloc(y*sizeof(int));
+	grid = (int **)malloc(y*sizeof(int*));
+	for(int c = 0; c < y; c++) grid[c] = (int*)malloc(x*sizeof(int));
 }//allocate_2d_mem
+
+void initialize_values(void){
+//initialize values in grid[][] to ASCII value of "o"
+	for(int i = 0; i < y; i++)
+		for(int j = 0; j < x; j++)
+			grid[i][j] = (int) 111;
+}//initialize_values
+
+void print_init(void){
+	for(int i = 0; i < y; i++){
+		for(int j = 0; j < x; j++)
+			printf("%c ", (char) grid[i][j]);
+
+		printf("\n");
+	}
+	
+}//print_init
 
 
 int main(int argc, char *argv[]) {
@@ -30,10 +47,11 @@ int main(int argc, char *argv[]) {
 	
 	if(!input) exit(EXIT_FAILURE);
 
-	int** grid; 
 	
-	allocate_2d_mem(&grid);
-
+	
+	allocate_2d_mem();
+	initialize_values();
+	print_init();
 
 	fclose(input);
 
