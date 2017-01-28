bb17ebd745ab713713751dc7aede06deeb19863b
Mitch <gainesmitch2@gmail.com>
2017-01-28 14:32:27 -0500

    allocated mem for variable amount of arrays and assigned them all to same values

--- PA2_jmgaines/PA2.c
+++ PA2_jmgaines/PA2.c
@@ -6,49 +6,45 @@
 #include "operations.h"
 
 FILE *input;
-int x, y, gens, init_x = 0, init_y = 0;
+int x, y, gens;
 char buff[255];
 int print, pause;
 int **grid, **variable_grid, **prev1, **prev2; 
 
 
-int** allocate_2d_mem(void){
+int** allocate_2d_mem(FILE *init_config){
 	//allocates memory for the 2d array
 
 	int i, **arr;
 	arr = malloc(y*sizeof(int*));
 	for(i = 0; i < y; i++) arr[i] = malloc(x*sizeof(int));
 
-	return arr;
-}//allocate_2d_mem
 
-void initialize_values(FILE *init_config){
-	//initialize values in grid[][] to ASCII value of "o"
 	for(int i = 0; i < y; i++)
 		for(int j = 0; j < x; j++)
-			grid[i][j] = (int) 111;
+			arr[i][j] = (int) 111;
 
-
-	//overwrite with init configeration and center it in grid
+	
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
+	int init_y = 0;
 	int x_assign = 0, y_assign = 0;
 	int x_start = 0, y_start = 0;
 	int x_greatest = 0;
 	int line_lengths[y];
-
+	
+	
 	for(int i = 0; i < 2; i++) {
 		while((read = getline(&line, &len, init_config)) != -1){
 			if(!i) { // gets length and width of file matrix
-				//init_x = !init_y ? read-1 : init_x;
 				line_lengths[init_y] = read-1;
 				x_greatest = x_greatest < read-1 ? read-1 : x_greatest;
 
 				init_y++;
 			} else { // assigns init values to center of grid array
 				for(x_assign = x_start; x_assign < line_lengths[y_assign-y_start] + x_start; x_assign++)
-					grid[y_assign][x_assign] = line[x_assign-x_start];
+					arr[y_assign][x_assign] = line[x_assign-x_start];
 				y_assign++;	
 			}
 		}
@@ -58,12 +54,17 @@ void initialize_values(FILE *init_config){
 		y_assign = y_start;
 	}
 
-}//initialize_values
+
+	return arr;
+}//allocate_2d_mem
+
+
+
 
 void print_grid(void){
 	for(int i = 0; i < y; i++){
 		for(int j = 0; j < x; j++)
-			printf("%c ", (char) grid[i][j]);
+			printf("%c ", (char) prev1[i][j]);
 
 		printf("\n");
 	}
@@ -81,13 +82,10 @@ int main(int argc, char *argv[]) {
 	
 	if(!input) exit(EXIT_FAILURE);
 
-	grid 	= allocate_2d_mem();		prev1 = allocate_2d_mem(); 
-	variable_grid = allocate_2d_mem();	prev2 = allocate_2d_mem();
-
-	allocate_2d_mem();
-	initialize_values(input);
-	print_grid();
+	grid 	= allocate_2d_mem(input);		prev1 = allocate_2d_mem(input); 
+	variable_grid = allocate_2d_mem(input);		prev2 = allocate_2d_mem(input);
 	
+	print_grid();	
 
 	fclose(input);
 
